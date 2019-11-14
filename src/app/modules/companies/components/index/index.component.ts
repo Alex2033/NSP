dcf95bdc081ad6b37ab728837d30c0339fb712ec
Alexandr <XoxlofAlexandr@yandex.ru>
2019-11-14 17:35:53 +0300

    изменена высота карточек персон и компаний, добавлен глобальный стиль для иконок стрелки, сверстана главная страница компаний, добавлена конфигурация для карточек компаний, добавлен первоначальный вид селекта в компонент поиска

--- src/app/modules/companies/components/index/index.component.ts
+++ src/app/modules/companies/components/index/index.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
+  companies = [ 
+    { image: 'https://images.unsplash.com/photo-1556834948-113a097c00eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80', title: 'БестЪ. Коммерческая недвижимость', phone: '+7 (812) 380–03–55', site: 'bestgroup.ru', tag: 'Агентство недвижимости' }, 
+    { image: 'https://images.unsplash.com/photo-1556834948-113a097c00eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80', title: 'Комплекс апарт-отелей VALO', phone: '+7 (812) 411–00–00', site: 'valoapart.ru', tag: 'Агентство недвижимости' }, 
+    { image: 'https://images.unsplash.com/photo-1556834948-113a097c00eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80', title: 'ООО ИСК Отделстрой', phone: '+7 (812) 596-59-70', site: 'otdelstroy.spb.ru', tag: 'Экспертиза' }, 
+    { image: 'https://images.unsplash.com/photo-1556834948-113a097c00eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80', title: 'Группа ЦДС', phone: '+7 (812) 320–12–00', site: 'cds.spb.ru', tag: 'Строительные материалы/технологии' }, 
+  ];
+
+  selectConfig = {
+    name: 'Вид деятельности',
+    options: {
+      0: 'Вариант 1',
+      1: 'Вариант 2',
+      2: 'Вариант 3'
+    }
+  };
+
   constructor() { }
 
   ngOnInit() {
