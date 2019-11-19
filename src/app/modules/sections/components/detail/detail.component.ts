67b6bf25cbdad7eb5b8c172f91dc598a064593f9
Alexandr <XoxlofAlexandr@yandex.ru>
2019-11-19 23:03:58 +0300

    сверстана детальная страница секций,  стили классов тегов перенесены из глобальной зоны видимости в карточки

--- src/app/modules/sections/components/detail/detail.component.ts
+++ src/app/modules/sections/components/detail/detail.component.ts
@@ -7,6 +7,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
+  tags = ['Апартаменты', 'Инвестиции', 'Ипотека', 'Законодательство', 'Частныйсектор'];
+
+  cards = [
+    {
+      type: 'front'
+    },
+    {
+      type: 'material',
+      size: 'small',
+      view: 'default'
+    },
+    {
+      type: 'material-list',
+      size: 'small',
+      view: 'default',
+      position: {
+        x: 4,
+        y: 1
+      }
+    },
+    {
+      type: 'latest-news',
+      position: {
+        x: 1,
+        y: 2
+      }
+    },
+    {
+      type: 'material',
+      size: 'small',
+      view: 'default'
+    },
+    {
+      type: 'material',
+      size: 'small',
+      view: 'default'
+    },
+    {
+      type: 'advertising',
+      size: 'small'
+    },
+    {
+      type: 'video',
+      size: 'medium-horizontal'
+    },
+    {
+      type: 'material-list',
+      size: 'medium-vertical',
+      view: 'readBlock',
+      position: {
+        x: 4,
+        y: 3
+      }
+    },
+    {
+      type: 'material',
+      size: 'small',
+      view: 'default'
+    },
+    {
+      type: 'material',
+      size: 'small',
+      view: 'backgroundImage'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
