6e1aa6eb7053422ca0f1fdb064831cf620bcdd71
Alexandr <XoxlofAlexandr@yandex.ru>
2020-03-15 22:37:53 +0300

    адаптирована карточка front для мобильных экранов

--- src/app/shared/components/cards/front-card/front-card.component.ts
+++ src/app/shared/components/cards/front-card/front-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResponsiveService } from 'src/app/shared/services/responsive.service';
 
 @Component({
   selector: 'app-front-card',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./front-card.component.scss']
 })
 export class FrontCardComponent implements OnInit {
+  screen: string;
 
-  constructor() { }
+  constructor(private responsive: ResponsiveService) { }
 
   ngOnInit() {
+    this.responsive.screen.subscribe((screen) => {
+      this.screen = screen;
+    });
   }
 
 }
