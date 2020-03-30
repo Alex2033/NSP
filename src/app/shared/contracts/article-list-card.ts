ac7fa2ccaf08b42e527a6a9e897fb79b8fb0f749
Dmitriy Tretyakov <dmitriy.tretyakov.work@gmail.com>
2020-03-30 18:35:12 +0300

    Запрограммировал плитку со списком статей

---
+++ src/app/shared/contracts/article-list-card.ts
@@ -0,0 +1,11 @@
+export interface ArticleListCard {
+  title: string;
+  highlight: boolean;
+  articles: {
+    id: number,
+    slug: string,
+    publishedAt: number,
+    title: string,
+    directorySlug: string
+  }[];
+}
