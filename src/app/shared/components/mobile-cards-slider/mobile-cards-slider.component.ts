321f75134fc830cc27517da526673d0afcf4b432
Dmitriy Tretyakov <dmitriy.tretyakov.work@gmail.com>
2020-06-24 15:47:51 +0300

    Исправил страницу персон на мобильном

--- src/app/shared/components/mobile-cards-slider/mobile-cards-slider.component.ts
+++ src/app/shared/components/mobile-cards-slider/mobile-cards-slider.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-mobile-cards-slider',
@@ -9,16 +10,19 @@ import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 export class MobileCardsSliderComponent implements OnInit {
   @Input() cards;
   @Input() type;
-
+  index;
   carouselConfig: SwiperConfigInterface = {
     slidesPerView: 'auto',
     spaceBetween: 8,
     loop: false
   };
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(() => {
+      this.index = 0;
+    });
   }
 
 }
