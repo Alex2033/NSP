f5ebfc8c9ef335bd8555a412604c33a7d88d0ae5
Dmitriy Tretyakov <dmitriy.tretyakov.work@gmail.com>
2020-07-03 15:12:47 +0300

    Поправил вход через соц. сети

--- src/app/modules/login/components/social-callback/social-callback.component.ts
+++ src/app/modules/login/components/social-callback/social-callback.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {finalize} from 'rxjs/operators';
 import {AuthService} from '../../../../shared/services/auth.service';
 import {LoaderService} from '../../../../shared/services/loader.service';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-social-callback',
@@ -13,19 +14,21 @@ export class SocialCallbackComponent implements OnInit {
   success = false;
   error;
 
-  constructor(private route: ActivatedRoute, private auth: AuthService,  private loader: LoaderService) {
+  constructor(private route: ActivatedRoute, private auth: AuthService,  private loader: LoaderService, @Inject(PLATFORM_ID) private platformId: any) {
   }
 
   ngOnInit() {
-    this.loader.hidePanels();
-    this.auth.loginViaOAuth(this.route.snapshot.params.provider, this.route.snapshot.queryParams).pipe(finalize(() => {
-      // this.loader.hide();
-    })).subscribe((response) => {
-      this.success = true;
-      //window.close();
-    }, (response) => {
-      this.error = response;
-    });
+    if(isPlatformBrowser(this.platformId)) {
+      this.loader.hidePanels();
+      this.auth.loginViaOAuth(this.route.snapshot.params.provider, this.route.snapshot.queryParams).pipe(finalize(() => {
+        // this.loader.hide();
+      })).subscribe((response) => {
+        this.success = true;
+        //window.close();
+      }, (response) => {
+        this.error = response;
+      });
+    }
   }
 
 }
