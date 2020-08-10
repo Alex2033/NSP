77a55bde0f9c6c77a35b5da133aa9fd7b21863d7
Dmitriy Tretyakov <dmitriy.tretyakov.work@gmail.com>
2020-08-10 17:16:20 +0300

    Фикс

--- server.ts
+++ server.ts
@@ -7,7 +7,6 @@ import 'zone.js/dist/zone-node';
 import {ngExpressEngine} from '@nguniversal/express-engine';
 import * as express from 'express';
 import {join} from 'path';
-import {robots} from 'express-robots-txt';
 import {AppServerModule} from './src/main.server';
 import {APP_BASE_HREF} from '@angular/common';
 import {existsSync} from 'fs';
