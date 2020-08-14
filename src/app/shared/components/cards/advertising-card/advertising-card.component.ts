4c790335f83f373835c61a86d53ebe5c66a1f69e
Dmitriy Tretyakov <dmitriy.tretyakov.work@gmail.com>
2020-08-14 21:34:15 +0300

    Добавил HTML баннеры в плитках

--- src/app/shared/components/cards/advertising-card/advertising-card.component.ts
+++ src/app/shared/components/cards/advertising-card/advertising-card.component.ts
@@ -3,6 +3,8 @@ import { ResizableCardComponent } from '../resizable-card.component';
 import {AdvertisingCard} from '../../../contracts/advertising-card';
 import {ApiService} from '../../../services/api.service';
 import {isPlatformBrowser} from '@angular/common';
+import {environment} from '../../../../../environments/environment';
+import {ResponsiveService} from '../../../services/responsive.service';
 
 @Component({
   selector: 'app-advertising-card',
@@ -10,9 +12,33 @@ import {isPlatformBrowser} from '@angular/common';
   styleUrls: ['./advertising-card.component.scss']
 })
 export class AdvertisingCardComponent extends ResizableCardComponent implements OnInit, AfterViewInit {
-
-  constructor(private api: ApiService, @Inject(PLATFORM_ID) private platformId: any) {
+  @ViewChild('link') link;
+  screen: any;
+  id: string;
+  constructor(private api: ApiService, @Inject(PLATFORM_ID) private platformId: any, private responsive: ResponsiveService) {
     super();
+    if(isPlatformBrowser(this.platformId)) {
+      const eventMethod = window.addEventListener
+        ? 'addEventListener'
+        : 'attachEvent';
+      const eventer = window[eventMethod];
+      const messageEvent = eventMethod === 'attachEvent'
+        ? 'onmessage'
+        : 'message';
+
+      eventer(messageEvent, (e) => {
+
+        if (e.origin !== environment.apiHost) {
+          return;
+        }
+        if (e.data.type === 'banner_click' && e.data.id === this.id) {
+          this.link.nativeElement.click();
+        }
+
+        // console.log(e);
+      });
+      this.id = Math.random().toString(36).substr(2, 9);
+    }
   }
 
   clicked = false;
@@ -25,6 +51,9 @@ export class AdvertisingCardComponent extends ResizableCardComponent implements 
   @ViewChild('htmlSmWrapper') htmlSmWrapper;
 
   ngOnInit() {
+    this.responsive.screen.subscribe((screen) => {
+      this.screen = screen;
+    });
   }
 
   ngAfterViewInit() {
@@ -53,6 +82,19 @@ export class AdvertisingCardComponent extends ResizableCardComponent implements 
     if (!this.clicked) {
       this.clicked = true;
       this.api.addAdvertisingCardClick(this.data.id).subscribe();
+    }
+  }
+
+  getScale() {
+    if (this.screen !== 'sm' || !window) {
+      return null;
+    } else {
+      return 'scale(' + ((window.outerWidth / 743) - (24 / 768)) + ')';
+    }
+  }
+  getBannerHeight() {
+    if (this.screen === 'sm' && window) {
+      return window.outerWidth - 24; // 24 это padding по краям
     }
   }
 }
